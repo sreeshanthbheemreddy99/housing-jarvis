564f06a7bafbf4bff220266404d861d09065ac30
Sreeshanth Bheemreddy <sreeshanthbheemreddy@Sreeshanths-MacBook-Air.local>
2025-05-28 12:09:30 -0500

    Initial commit: Angular Housing Project setup

---
+++ src/app/models/property.ts
@@ -0,0 +1,12 @@
+export interface Property {
+    id: number;
+    name: string;
+    type: string;
+    price: number;
+    location: string;
+    image: string;
+    bedrooms: number;
+    bathrooms: number;
+    area: number;
+    description: string;
+}
